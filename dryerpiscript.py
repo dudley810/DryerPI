c8096107a9fcc609728ed21ea2f8c0966452f185
dudley.kenneth <kenneth.dudley@level3.com>
2016-04-05 23:58:27 -0400

    when circuit is triggered does not always goto the false therefore the timer is the only way it can be done.  Removing the notify count.

--- dryerpiscript.py
+++ dryerpiscript.py
@@ -8,57 +8,41 @@ import os
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(4,GPIO.IN)
-global NotifyCount
 global lastemail
-global lastprint
 NotificationSeconds = 600
-NotifyCount = 0
-lastemail = datetime.datetime.now()
-lastprint = datetime.datetime.now()
+lastemail = datetime.datetime.now()-datetime.timedelta(seconds=700)
 r = req.post("http://192.168.1.200:8080/api/startpi")
 
 def CheckPrint(Message):
-   global NotifyCount
-   global lastprint
    global lastemail
-   current = datetime.datetime.now()
-   diffvalue = current - lastprint
-   if (diffvalue.seconds > 1):
-        lastprint = datetime.datetime.now() 
-        print("%s-GPIO=%s, Lastemail=%s, Current=%s, Notified=%s  " % (Message,GPIO.input(4), lastemail,current,NotifyCount))
+   #print("%s-GPIO=%s, Lastemail=%s, Current=%s  " % (Message,GPIO.input(4), lastemail,datetime.datetime.now()))
    return
 
 # Dryer Done
 def DryerDone():
-   global NotifyCount
    global lastemail
    payload = {'gpioresult': 0}
    r = req.post("http://192.168.1.200:8080/api/dryerpi", params=payload)
    if (r.status_code == req.codes.ok):
-       NotifyCount += 1
        lastemail = datetime.datetime.now()
-       #current = datetime.datetime.now()
-       #print(r.json())
-       #print("Dryer Done GPIO=%s,Lastemail=%s, Current=%s, Notified=%s " % (GPIO.input(4), lastemail,current,NotifyCount))
+       #print("Dryer Done GPIO=%s,Lastemail=%s, Current=%s " % (GPIO.input(4), lastemail,datetime.datetime.now()))
    return
 
 while True:
+    sleep(1)
     current = datetime.datetime.now()
     if (current.hour > 22):
         r = req.post("http://192.168.1.200:8080/api/stoppi")
-        print("Past 10pm now shuting down PI")
+       #print("Past 11pm now shuting down PI")
         os.system('shutdown now -h')
         exit()
     else: 
         if (GPIO.input(4)):
-            NotifyCount = 0
-            #CheckPrint("Waiting-") 
+            continue
+           #CheckPrint("Waiting-") 
         else:
-            if (NotifyCount == 0):
+            diffvalue = current - lastemail  
+            if (diffvalue.seconds > NotificationSeconds):  
                 DryerDone()
-            else:
-                diffvalue = current - lastemail  
-                if (diffvalue.seconds > NotificationSeconds and NotifyCount < 3):  
-                     DryerDone()
-                #else:
-                #     CheckPrint("Not Time Yet-") 
+           #else:
+           #    CheckPrint("Not Time Yet-") 
